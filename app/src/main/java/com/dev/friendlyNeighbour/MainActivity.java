54caef6891e99b17488010721613cf2ceeaef22b
Ritwik <ritwik1499@gmail.com>
2020-05-11 15:11:33 +0530

    Added Location and Volley posts.

--- app/src/main/java/com/dev/friendlyNeighbour/MainActivity.java
+++ app/src/main/java/com/dev/friendlyNeighbour/MainActivity.java
@@ -2,13 +2,24 @@ package com.dev.friendlyNeighbour;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        next  = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, sendLocation.class );
+                startActivity(intent);
+            }
+        } );
     }
 }
