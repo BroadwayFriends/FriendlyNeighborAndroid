58de1de3637883b976361cd54027a9162617de14
Ritwik <ritwik1499@gmail.com>
2020-05-13 08:59:57 +0530

    Added requirements activity

---
+++ app/src/main/java/me/twodee/friendlyneighbor/postRequirementActivity.java
@@ -0,0 +1,14 @@
+package me.twodee.friendlyneighbor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class postRequirementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_requirement);
+    }
+}
