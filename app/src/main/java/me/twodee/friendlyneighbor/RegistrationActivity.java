78751581c070966d94e79455704fd2b2617cdc32
Priyam Sahoo <priyamsahoo.26@gmail.com>
2020-05-14 12:31:48 +0530

    Valdation of form done, sending user data pending

--- app/src/main/java/me/twodee/friendlyneighbor/RegistrationActivity.java
+++ app/src/main/java/me/twodee/friendlyneighbor/RegistrationActivity.java
@@ -12,21 +12,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.regex.Pattern;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    TextInputLayout addr1;
-    TextInputLayout addr2;
-    TextInputLayout city;
-    TextInputLayout state;
-    TextInputLayout country;
-    TextInputLayout pincode;
-    TextInputLayout contactNumber;
-
+    TextInputLayout addr1, addr2, city, state, country, pincode, contactNumber;
     Button submitButton;
+
+    AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,63 +43,77 @@ public class RegistrationActivity extends AppCompatActivity {
         contactNumber = findViewById(R.id.contact);
         submitButton = findViewById(R.id.submitButton);
 
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        //Validations
+        awesomeValidation.addValidation(this, R.id.addressLine1, RegexTemplate.NOT_EMPTY, R.string.invalid_address_1);
+        awesomeValidation.addValidation(this, R.id.city, RegexTemplate.NOT_EMPTY, R.string.invalid_city);
+        awesomeValidation.addValidation(this, R.id.state, RegexTemplate.NOT_EMPTY, R.string.invalid_state);
+        awesomeValidation.addValidation(this, R.id.country, RegexTemplate.NOT_EMPTY, R.string.invalid_country);
+        awesomeValidation.addValidation(this, R.id.pincode, "[1-9][0-9]{5}$", R.string.invalid_pincode);
+        awesomeValidation.addValidation(this, R.id.contact, "(0/91)?[7-9][0-9]{9}", R.string.invalid_contact_number);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkDataEntered();
+                if(awesomeValidation.validate()) {
+                    Toast.makeText(RegistrationActivity.this, "Success !!!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(RegistrationActivity.this, "Failed", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
-    void checkDataEntered() {
-        if (isEmpty(addr1)) {
-//            Toast.makeText(RegistrationActivity.this, "Address Line 1 cannot be empty !", Toast.LENGTH_LONG).show();
-            addr1.setError("Address Line 1 Required");
-        }
-
-        if (isEmpty(city)) {
-//            Toast.makeText(RegistrationActivity.this, "City cannot be empty !", Toast.LENGTH_LONG).show();
-            city.setError("City Required");
-        }
-
-        if (isEmpty(state)) {
-//            Toast.makeText(RegistrationActivity.this, "State cannot be empty !", Toast.LENGTH_LONG).show();
-            state.setError("State Required");
-        }
-
-        if (isEmpty(country)) {
-//            Toast.makeText(RegistrationActivity.this, "Country cannot be empty !", Toast.LENGTH_LONG).show();
-            country.setError("Country Required");
-        }
-
-
-        if (!Pattern.matches("[1-9][0-9]{5}$", pincode.getEditText().getText().toString())) {
-//            Toast.makeText(RegistrationActivity.this, "Invalid Pin Code !", Toast.LENGTH_LONG).show();
-            pincode.setError("Invalid Pin Code");
-        }
-
-//        if (pincode.toString().matches("^[0-9]{6}$")) {
-//            Toast.makeText(RegistrationActivity.this, "Invalid Pin code !", Toast.LENGTH_LONG).show();
+//    void checkDataEntered() {
+//        if (isEmpty(addr1)) {
+////            Toast.makeText(RegistrationActivity.this, "Address Line 1 cannot be empty !", Toast.LENGTH_LONG).show();
+//            addr1.setError("Address Line 1 Required");
 //        }
-
-        if (!Pattern.matches("(0/91)?[7-9][0-9]{9}", contactNumber.getEditText().getText().toString())) {
-//            Toast.makeText(RegistrationActivity.this, "Invalid Contact Number !", Toast.LENGTH_LONG).show();
-            contactNumber.setError("Invalid Contact Number");
-        }
-    }
-
-    boolean isEmpty(TextInputLayout text) {
-        CharSequence str = text.getEditText().getText().toString();
-        return TextUtils.isEmpty(str);
-    }
-
-    boolean isPinCode(TextInputLayout text) {
-        CharSequence pincode = text.getEditText().getText().toString();
-        return (!TextUtils.isEmpty(pincode) && !Pattern.matches("[0-9]{6}$", pincode));
-    }
-
-    boolean isContact(TextInputLayout text) {
-        CharSequence con = text.getEditText().getText().toString();
-        return (!TextUtils.isEmpty(con) && Patterns.PHONE.matcher(con).matches());
-    }
+//
+//        if (isEmpty(city)) {
+////            Toast.makeText(RegistrationActivity.this, "City cannot be empty !", Toast.LENGTH_LONG).show();
+//            city.setError("City Required");
+//        }
+//
+//        if (isEmpty(state)) {
+////            Toast.makeText(RegistrationActivity.this, "State cannot be empty !", Toast.LENGTH_LONG).show();
+//            state.setError("State Required");
+//        }
+//
+//        if (isEmpty(country)) {
+////            Toast.makeText(RegistrationActivity.this, "Country cannot be empty !", Toast.LENGTH_LONG).show();
+//            country.setError("Country Required");
+//        }
+//
+//
+//        if (!Pattern.matches("[1-9][0-9]{5}$", pincode.getEditText().getText().toString())) {
+////            Toast.makeText(RegistrationActivity.this, "Invalid Pin Code !", Toast.LENGTH_LONG).show();
+//            pincode.setError("Invalid Pin Code");
+//        }
+//
+////        if (pincode.toString().matches("^[0-9]{6}$")) {
+////            Toast.makeText(RegistrationActivity.this, "Invalid Pin code !", Toast.LENGTH_LONG).show();
+////        }
+//
+//        if (!Pattern.matches("(0/91)?[7-9][0-9]{9}", contactNumber.getEditText().getText().toString())) {
+////            Toast.makeText(RegistrationActivity.this, "Invalid Contact Number !", Toast.LENGTH_LONG).show();
+//            contactNumber.setError("Invalid Contact Number");
+//        }
+//    }
+//
+//    boolean isEmpty(TextInputLayout text) {
+//        CharSequence str = text.getEditText().getText().toString();
+//        return TextUtils.isEmpty(str);
+//    }
+//
+//    boolean isPinCode(TextInputLayout text) {
+//        CharSequence pincode = text.getEditText().getText().toString();
+//        return (!TextUtils.isEmpty(pincode) && !Pattern.matches("[0-9]{6}$", pincode));
+//    }
+//
+//    boolean isContact(TextInputLayout text) {
+//        CharSequence con = text.getEditText().getText().toString();
+//        return (!TextUtils.isEmpty(con) && Patterns.PHONE.matcher(con).matches());
+//    }
 }
