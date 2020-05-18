09198f707de63d4ae2867b8eb5e86d0e97f7e907
Ritwik <ritwik1499@gmail.com>
2020-05-18 21:18:51 +0530

    finishing touches to requests

--- app/src/main/java/me/twodee/friendlyneighbor/imageUploadActivity.kt
+++ app/src/main/java/me/twodee/friendlyneighbor/imageUploadActivity.kt
@@ -26,7 +26,7 @@ class imageUploadActivity : AppCompatActivity() {
         // Choose the number which is good for you, here I'll use a random one.
         const val pickFileRequestCode = 69
         private const val TAG = "uploadData"
-        const val baseUrl = "https://ptsv2.com/t/pt68f-1589802042/post"
+        const val baseUrl = "https://4112a99e.ngrok.io/api/requests"
         var image1: Uri = Uri.EMPTY
         var image2: Uri = Uri.EMPTY
         var image3: Uri = Uri.EMPTY
@@ -89,78 +89,85 @@ class imageUploadActivity : AppCompatActivity() {
         var reqObj = MultipartUploadRequest(this, baseUrl)
                 .setMethod("POST")
                 .addParameter("data", JSONObject(data as Map<*, *>).toString())
+                .addHeader("_id","5ec166d582bc531c24eba282")
 
-
-                when (imageCount) {
-                        1 -> {
-                            image1 = Uri.parse(imageUriArray?.get(0))
-                            reqObj.addFileToUpload(filePath = image1.toString(), parameterName = "image1")
-                            Log.v(TAG,"Only 1 image selected")
-                        }
-                        2 -> {
-                            image1 = Uri.parse(imageUriArray?.get(0))
-                            image2 = Uri.parse(imageUriArray?.get(1))
-                            reqObj.addFileToUpload(filePath = image1.toString(), parameterName = "image1")
-                            reqObj.addFileToUpload(filePath = image2.toString(), parameterName = "image2")
-                            Log.v(TAG,"Only 2 images selected")
-                        }
-                        3 -> {
-                            image1 = Uri.parse(imageUriArray?.get(0))
-                            image2 = Uri.parse(imageUriArray?.get(1))
-                            image3 = Uri.parse(imageUriArray?.get(2))
-                            reqObj.addFileToUpload(filePath = image1.toString(), parameterName = "image1")
-                            reqObj.addFileToUpload(filePath = image2.toString(), parameterName = "image2")
-                            reqObj.addFileToUpload(filePath = image3.toString(), parameterName = "image3")
-                            Log.v(TAG,"All 3 selected")
-                        }
-                 }
-
-
-
-                reqObj.subscribe(context = context, lifecycleOwner = lifecycleOwner, delegate = object : RequestObserverDelegate {
-                    override fun onProgress(context: Context, uploadInfo: UploadInfo) {
-                        Toast.makeText(applicationContext,"Uploading !!",Toast.LENGTH_SHORT)
-                    }
-
-                    override fun onSuccess(
-                            context: Context,
-                            uploadInfo: UploadInfo,
-                            serverResponse: ServerResponse
-                    ) {
-                        // do your thing
-                        Log.i(TAG, "Success: ${serverResponse.bodyString}")
-
-
-                    }
-
-                    override fun onError(context: Context, uploadInfo: UploadInfo, exception: Throwable) {
-                        when (exception) {
-                            is UserCancelledUploadException -> {
-                                Log.e(TAG, "Error, user cancelled upload: $uploadInfo")
+//
+//                when (imageCount) {
+//                        1 -> {
+//                            image1 = Uri.parse(imageUriArray?.get(0))
+//                            reqObj.addFileToUpload(filePath = image1.toString(), parameterName = "image1")
+//                            Log.v(TAG,"Only 1 image selected")
+//                        }
+//                        2 -> {
+//                            image1 = Uri.parse(imageUriArray?.get(0))
+//                            image2 = Uri.parse(imageUriArray?.get(1))
+//                            reqObj.addFileToUpload(filePath = image1.toString(), parameterName = "image1")
+//                            reqObj.addFileToUpload(filePath = image2.toString(), parameterName = "image2")
+//                            Log.v(TAG,"Only 2 images selected")
+//                        }
+//                        3 -> {
+//                            image1 = Uri.parse(imageUriArray?.get(0))
+//                            image2 = Uri.parse(imageUriArray?.get(1))
+//                            image3 = Uri.parse(imageUriArray?.get(2))
+//                            reqObj.addFileToUpload(filePath = image1.toString(), parameterName = "image1")
+//                            reqObj.addFileToUpload(filePath = image2.toString(), parameterName = "image2")
+//                            reqObj.addFileToUpload(filePath = image3.toString(), parameterName = "image3")
+//                            Log.v(TAG,"All 3 selected")
+//                        }
+//                    else -> {
+////
+//                    }
+//                 }
+            .subscribe(context = context, lifecycleOwner = lifecycleOwner, delegate = object : RequestObserverDelegate {
+                            override fun onProgress(context: Context, uploadInfo: UploadInfo) {
+                                Toast.makeText(applicationContext,"Uploading !!",Toast.LENGTH_SHORT)
                             }
 
-                            is UploadError -> {
-                                Log.e(TAG, "Error, upload error: ${exception.serverResponse.bodyString}")
+                            override fun onSuccess(
+                                    context: Context,
+                                    uploadInfo: UploadInfo,
+                                    serverResponse: ServerResponse
+                            ) {
+                                // do your thing
+                                Toast.makeText(this@imageUploadActivity,"${serverResponse.bodyString}",Toast.LENGTH_SHORT).show();
+                                Log.i(TAG, "Success: ${serverResponse.bodyString}")
+
+
                             }
 
-                            else -> {
-                                Log.e(TAG, "Error: $uploadInfo", exception)
+                            override fun onError(context: Context, uploadInfo: UploadInfo, exception: Throwable) {
+                                when (exception) {
+                                    is UserCancelledUploadException -> {
+                                        Log.e(TAG, "Error, user cancelled upload: $uploadInfo")
+                                    }
+
+                                    is UploadError -> {
+                                        Log.e(TAG, "Error, upload error: ${exception.serverResponse.bodyString}")
+                                    }
+
+                                    else -> {
+                                        Log.e(TAG, "Error: $uploadInfo", exception)
+
+                                    }
+                                }
+                            }
+
+                            override fun onCompleted(context: Context, uploadInfo: UploadInfo) {
+                                Toast.makeText(this@imageUploadActivity,"Successfully posted your request!",Toast.LENGTH_SHORT).show();
+
 
                             }
-                        }
-                    }
 
-                    override fun onCompleted(context: Context, uploadInfo: UploadInfo) {
-                        Toast.makeText(this@imageUploadActivity,"Successfully posted your request!",Toast.LENGTH_SHORT).show();
+                            override fun onCompletedWhileNotObserving() {
+
+                                // do your thing
+                            }
+                        })
 
 
-                    }
 
-                    override fun onCompletedWhileNotObserving() {
 
-                        // do your thing
-                    }
-                })
+
 
 
 
