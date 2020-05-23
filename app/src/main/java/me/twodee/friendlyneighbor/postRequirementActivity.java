e960a62bbd0024ed3835e8631f3ec012101e2ced
Ritwik <ritwik1499@gmail.com>
2020-05-23 18:36:02 +0530

    Adding Requests UI

--- app/src/main/java/me/twodee/friendlyneighbor/postRequirementActivity.java
+++ app/src/main/java/me/twodee/friendlyneighbor/postRequirementActivity.java
@@ -121,7 +121,7 @@ public class postRequirementActivity extends AppCompatActivity {
 //        awesomeValidation.addValidation(this, R.id.editTextDob, "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$", R.string.nameerror);
 
 
-//        TODO: TAKE DATA FROM PREVIOUS ACTIVITY (uID,phoneNumber,Home location,distance)
+//        TODO: take data from previous activity (uID,phoneNumber,Home location,distance)
 
 
         buttonImageUpload.setOnClickListener(new View.OnClickListener()
