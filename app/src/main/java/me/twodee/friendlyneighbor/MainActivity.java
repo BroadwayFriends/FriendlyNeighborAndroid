281e79a2fbac8a9aa0b0d8937de036305c08814e
Priyam Sahoo <priyamsahoo.26@gmail.com>
2020-05-24 10:13:28 +0530

    Made the flow

--- app/src/main/java/me/twodee/friendlyneighbor/MainActivity.java
+++ app/src/main/java/me/twodee/friendlyneighbor/MainActivity.java
@@ -33,30 +33,31 @@ public class MainActivity extends AppCompatActivity
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_main);
-        discover = findViewById(R.id.discover);
-        discover.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, DiscoverActivity.class);
-                startActivity(intent);
-            }
-        });
 
-        next = findViewById(R.id.next);
-        next.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
+//        discover = findViewById(R.id.discover);
+//        discover.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent intent = new Intent(MainActivity.this, DiscoverActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-               
-                Intent intent = new Intent(MainActivity.this, RespondToRequestActivity.class);
-
-                startActivity(intent);
-            }
-        });
+//        next = findViewById(R.id.next);
+//        next.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//
+//
+//                Intent intent = new Intent(MainActivity.this, RespondToRequestActivity.class);
+//
+//                startActivity(intent);
+//            }
+//        });
 
         signInPage = findViewById(R.id.sign_in_page);
         signInPage.setOnClickListener(new View.OnClickListener()
@@ -69,80 +70,80 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        dashboard = findViewById(R.id.dashoard);
-        dashboard.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-                startActivity(intent);
-            }
-        });
+//        dashboard = findViewById(R.id.dashoard);
+//        dashboard.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        register = findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-            }
-        });
+//        register = findViewById(R.id.register);
+//        register.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        payment = findViewById(R.id.payment);
-        payment.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, PaymentActivity.class);
-                startActivity(intent);
-            }
-        });
+//        payment = findViewById(R.id.payment);
+//        payment.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent intent = new Intent(MainActivity.this, PaymentActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        profile = findViewById(R.id.profile);
-        profile.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, EditProfileActivity.class);
-                startActivity(intent);
-            }
-        });
+//        profile = findViewById(R.id.profile);
+//        profile.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent intent = new Intent(MainActivity.this, EditProfileActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        postDetails = findViewById(R.id.postDetails);
-        postDetails.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, PostDetailsActivity.class);
-                startActivity(intent);
-            }
-        });
+//        postDetails = findViewById(R.id.postDetails);
+//        postDetails.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent intent = new Intent(MainActivity.this, PostDetailsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        historyButton = findViewById(R.id.historyButton);
-        historyButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
-                startActivity(intent);
-            }
-        });
+//        historyButton = findViewById(R.id.historyButton);
+//        historyButton.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        postReqButton = findViewById(R.id.post_req_button);
-        postReqButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, postRequirementActivity.class);
-                startActivity(intent);
-            }
-        });
+//        postReqButton = findViewById(R.id.post_req_button);
+//        postReqButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, postRequirementActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
 //        loadingButton = (LoadingButton) findViewById(R.id.loadingButton);
 //
