9d7ae8a298af42bbff3d408b6d268306e337fcbb
Priyam Sahoo <priyamsahoo.26@gmail.com>
2020-10-22 20:42:27 +0530

    URL updates

--- app/src/main/java/me/twodee/friendlyneighbor/imageUploadActivity.kt
+++ app/src/main/java/me/twodee/friendlyneighbor/imageUploadActivity.kt
@@ -35,7 +35,7 @@ class   imageUploadActivity : AppCompatActivity() {
         const val pickFileRequestCode = 69
         private const val TAG = "uploadData"
 //        const val baseUrl = "https://fn.twodee.me/api/requests"
-        const val baseUrl = "https://fbb679ac761f.ngrok.io/api/requests"
+        const val baseUrl = "https://7ca4c2765246.ngrok.io/api/requests"
 //        val baseUrl = Resources.getSystem().getString(R.string.base_url) + "/api/requests"
 //
         var image1: Uri = Uri.EMPTY
