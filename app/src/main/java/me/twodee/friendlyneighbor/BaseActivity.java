d0f437e66a524f0ae015e5d1467f92c5f8fb0e2d
Priyam Sahoo <priyamsahoo.26@gmail.com>
2020-11-17 10:44:42 +0530

    Implemented chip navigation, rectified back button problem in bottom nav bar

--- app/src/main/java/me/twodee/friendlyneighbor/BaseActivity.java
+++ app/src/main/java/me/twodee/friendlyneighbor/BaseActivity.java
@@ -1,6 +1,7 @@
 package me.twodee.friendlyneighbor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,26 +11,37 @@ import android.widget.TextView;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class BaseActivity extends AppCompatActivity  {
+
     private SharedPreferences preferences;
     private String userId;
+
+    ChipNavigationBar chipNavigationBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
-        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+//        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+//
+//        bottomNavigationBar
+//                .addItem(new BottomNavigationItem(R.drawable.pin2, "Discover"))
+//                .addItem(new BottomNavigationItem(R.drawable.pin3, "Karma"))
+//                .addItem(new BottomNavigationItem(R.drawable.pin4, "Profile"))
+//
+//                .setFirstSelectedPosition(0)
+//                .initialise();
 
-        bottomNavigationBar
-                .addItem(new BottomNavigationItem(R.drawable.pin2, "Discover"))
-                .addItem(new BottomNavigationItem(R.drawable.pin3, "Karma"))
-                .addItem(new BottomNavigationItem(R.drawable.pin4, "Profile"))
 
-                .setFirstSelectedPosition(0)
-                .initialise();
+        chipNavigationBar = findViewById(R.id.bottom_navigation_menu);
+        chipNavigationBar.setItemSelected(R.id.bottom_nav_discover, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new KarmaFragment()).commit();
+        bottomMenu();
 
         preferences = getSharedPreferences("UserDetails", MODE_PRIVATE);
         userId = preferences.getString("_id", null);
@@ -38,45 +50,70 @@ public class BaseActivity extends AppCompatActivity  {
 
 
 
-        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
+//        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
+//            @Override
+//            public void onTabSelected(int position) {
+//                switch (position) {
+//                    case 0:
+//
+//                        FragmentTransaction ft0 = getSupportFragmentManager().beginTransaction();
+//                        ft0.replace(R.id.mainFrame, new DiscoverFragment());
+//                        ft0.addToBackStack(null);
+//                        ft0.commit();
+//
+//                        break;
+//
+//                    case 1:
+//
+//                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//                        ft.replace(R.id.mainFrame, new KarmaFragment());
+//                        ft.addToBackStack(null);
+//                        ft.commit();
+//
+//                        break;
+//
+//                    case 2:
+//
+//                        FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+//                        ft1.replace(R.id.mainFrame, new ProfileFragment());
+//                        ft1.addToBackStack(null);
+//                        ft1.commit();
+//                        break;
+//                }
+//            }
+//            @Override
+//            public void onTabUnselected(int position) {
+//            }
+//            @Override
+//            public void onTabReselected(int position) {
+//            }
+//        });
+    }
+
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
             @Override
-            public void onTabSelected(int position) {
-                switch (position) {
-                    case 0:
-
-                        FragmentTransaction ft0 = getSupportFragmentManager().beginTransaction();
-                        ft0.replace(R.id.mainFrame, new DiscoverFragment());
-                        ft0.addToBackStack(null);
-                        ft0.commit();
-
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i) {
+                    case R.id.bottom_nav_discover:
+                        fragment = new DiscoverFragment();
                         break;
 
-                    case 1:
-
-                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                        ft.replace(R.id.mainFrame, new KarmaFragment());
-                        ft.addToBackStack(null);
-                        ft.commit();
-
+                    case R.id.bottom_nav_karma:
+                        fragment = new KarmaFragment();
                         break;
 
-                    case 2:
-
-                        FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
-                        ft1.replace(R.id.mainFrame, new ProfileFragment());
-                        ft1.addToBackStack(null);
-                        ft1.commit();
+                    case R.id.bottom_nav_profile:
+                        fragment = new ProfileFragment();
                         break;
                 }
-            }
-            @Override
-            public void onTabUnselected(int position) {
-            }
-            @Override
-            public void onTabReselected(int position) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
             }
         });
     }
+
     public HashMap<String, String> passDataToFrags(){
         HashMap<String, String> userData= new HashMap<>();
         userData.put("userId",userId);
