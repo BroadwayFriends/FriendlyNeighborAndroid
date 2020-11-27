77d26b6cebb6a4d8dacd0e7c91097736fb24c957
Priyam Sahoo <priyamsahoo.26@gmail.com>
2020-11-27 20:18:38 +0530

    Completed layout for transaction list view

---
+++ app/src/main/java/me/twodee/friendlyneighbor/TransactionOnGoingFragment.java
@@ -0,0 +1,82 @@
+package me.twodee.friendlyneighbor;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TransactionOnGoingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TransactionOnGoingFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public TransactionOnGoingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TransactionOnGoingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TransactionOnGoingFragment newInstance(String param1, String param2) {
+        TransactionOnGoingFragment fragment = new TransactionOnGoingFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    RecyclerView recyclerView;
+    DiscoverDetailsAdapter discoverDetailsAdapter;
+
+    List<DiscoverDetails> discoverDetailsList;
+
+    SearchView searchView;
+
+    SharedPreferences preferences;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaction_on_going, container, false);
+    }
+}
